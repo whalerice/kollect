c159307cce9bca1f3e49744f584d25159a374c50
joy <joyfulforest@gmail.com>
2022-05-02 22:39:18 +0900

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,60 @@
+(() => {
+	$('.main-slider').slick({
+		infinite: true,
+		dots: true,
+		arrows: false,
+	});
+
+	let ani = lottie.loadAnimation({
+		container: document.getElementById('lottie'),
+		renderer: 'svg',
+		loop: false,
+		autoplay: false,
+		path: 'https://assets3.lottiefiles.com/packages/lf20_0uqjzch2.json'
+		// path: '/images/slider/data1_in.json'
+	});
+
+	let ani2 = lottie.loadAnimation({
+		container: document.getElementById('lottie2'),
+		renderer: 'svg',
+		loop: false,
+		autoplay: false,
+		path: 'https://assets5.lottiefiles.com/packages/lf20_e6pyivz1.json'
+		// path: '/images/slider/data2_in.json'
+	});
+
+	ani.play();
+	$('.main-slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+		console.log(currentSlide);
+		currentSlide === 0 ? ani2.play() : ani2.stop();
+		currentSlide === 1 ? ani.play() : ani.stop();
+	});
+
+
+
+
+	// setTimeout(()=>{
+	// 	let iconSkipForward = document.querySelector('.bodymovinanim');
+	//
+	// 	let animationSkipForward = bodymovin.loadAnimation({
+	// 		container: iconSkipForward,
+	// 		renderer: 'svg',
+	// 		loop: true,
+	// 		autoplay: true,
+	// 		path: "https://assets3.lottiefiles.com/packages/lf20_0dkyba8h.json"
+	// 	});
+	//
+	// 	// iconSkipForward.addEventListener('click', function() {
+	// 	// 	animationSkipForward.playSegments([0,60], true);
+	// 	// });
+	// 	// animationSkipForward.playSegments([0,60], false);
+	// },100)
+
+
+	console.log('bb')
+
+
+	window.addEventListener("load", () => {});
+
+
+})();
