04d54142a6761131b95683d7e620af7f500e1b40
joy <joyfulforest@gmail.com>
2022-05-18 00:19:01 +0900

    canvas img

--- src/js/main.js
+++ src/js/main.js
@@ -6,60 +6,93 @@
 	let utillityimgPos2;
 	let utillityimgPos3;
 
-	function move() {
-		let i = 0;
-		if (i == 0) {
-			i = 1;
-			let elem = document.querySelector('#main-line');
-			let width = 1;
-			let id = setInterval(frame, 10);
-			function frame() {
-				if (width >= 100) {
-					clearInterval(id);
-					i = 0;
-					// ani.playSegments([0, 24]);
-					ani.play();
-					ani.playSegments([30,100]);
-				} else {
-					width++;
-					elem.style.width = width + "%";
-				}
-			}
+	// function move() {
+	// 	let i = 0;
+	// 	if (i == 0) {
+	// 		i = 1;
+	// 		let elem = document.querySelector('#main-line');
+	// 		let width = 1;
+	// 		let id = setInterval(frame, 10);
+	// 		function frame() {
+	// 			if (width >= 100) {
+	// 				clearInterval(id);
+	// 				i = 0;
+	// 				// ani.playSegments([0, 24]);
+	// 				ani.play();
+	// 				ani.playSegments([30,100]);
+	// 			} else {
+	// 				width++;
+	// 				elem.style.width = width + "%";
+	// 			}
+	// 		}
+	// 	}
+	// }
+
+	function plays (video,canvas) {
+		const sourcevideo = document.getElementById(video);
+		const outputcanvas = document.getElementById(canvas);
+		const canvasctx = outputcanvas.getContext("2d");
+
+		function initCanvas() {
+			outputcanvas.width = sourcevideo.videoWidth;
+			outputcanvas.height = sourcevideo.videoHeight;
+			drawVideo();
 		}
+
+		function drawVideo() {
+			canvasctx.drawImage(
+				sourcevideo,
+				0,
+				0,
+				outputcanvas.width,
+				outputcanvas.height
+			);
+			requestAnimationFrame(drawVideo);
+		}
+
+		sourcevideo.addEventListener("loadeddata", initCanvas);
 	}
+
 
 	$('.main-slider').slick({
 		infinite: true,
 		dots: true,
 		arrows: false,
-		autoplay: true,
-		autoplaySpeed: 10000,
+		autoplay: false,
+		autoplaySpeed: 9000,
 	});
 
-	let ani = lottie.loadAnimation({
-		container: document.getElementById('lottie'),
-		renderer: 'canvas',
-		loop: true,
-		autoplay: false,
-		// path: 'https://assets3.lottiefiles.com/packages/lf20_0uqjzch2.json'
-		path: '/images/slider/data_2.json'
-	});
-	let ani2 = lottie.loadAnimation({
-		container: document.getElementById('lottie2'),
-		renderer: 'canvas',
-		loop: true,
-		autoplay: false,
-		// path: 'https://assets5.lottiefiles.com/packages/lf20_e6pyivz1.json'
-		path: '/images/slider/data_3.json'
-	});
+	plays('sourcevideo','outputcanvas');
+	plays('sourcevideo2','outputcanvas2');
+
+	// let ani = lottie.loadAnimation({
+	// 	container: document.getElementById('lottie'),
+	// 	renderer: 'canvas',
+	// 	loop: true,
+	// 	autoplay: false,
+	// 	// path: 'https://assets3.lottiefiles.com/packages/lf20_0uqjzch2.json'
+	// 	path: '/images/slider/data_2.json'
+	// });
+	// let ani2 = lottie.loadAnimation({
+	// 	container: document.getElementById('lottie2'),
+	// 	renderer: 'canvas',
+	// 	loop: true,
+	// 	autoplay: false,
+	// 	// path: 'https://assets5.lottiefiles.com/packages/lf20_e6pyivz1.json'
+	// 	path: '/images/slider/data_3.json'
+	// });
 	// ani.setDirection(-1)
 	// ani2.setDirection(-1)
 
 	$('.main-slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+
 		// ani.goToAndPlay(1, true)
 		// ani.goToAndStop(5, true)
-		currentSlide === 0 ? ani2.play() : ani2.stop();
-		currentSlide === 1 ? move() : ani.stop();
+		// currentSlide === 0 ? ani2.play() : ani2.stop();
+		// currentSlide === 1 ? move() : ani.stop();
+
+		// currentSlide === 0 ? ani2.play() : ani2.stop();
+		// currentSlide === 1 ? move() : ani.stop();
 	});
 
 	function navToggle(e) {
@@ -243,11 +276,11 @@
 	}
 
 	window.addEventListener('resize', () =>{
-		lottie.resize();
+		// lottie.resize();
 	})
 	window.addEventListener("load", () => {
 		document.body.classList.remove('before-load');
-		move();
+		// move();
 		winW = window.innerWidth;
 		utillityimgPos1 = $('#utillityimgPos1').offset().top;
 		utillityimgPos2 = $('#utillityimgPos2').offset().top;
